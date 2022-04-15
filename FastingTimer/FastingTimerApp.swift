62cef2f0f35086e3a51f318f7b5b711d673b690f
Artem Paliutin <pal.artem@icloud.com>
2022-04-15 17:48:59 +0300

    Initial Commit

---
+++ FastingTimer/FastingTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastingTimerApp.swift
+//  FastingTimer
+//
+//  Created by Artem Paliutin on 15/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct FastingTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
